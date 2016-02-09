50b583d308da355de930f98b952b6dc3b302a691
Wojtek Czekalski <me@wczekalski.com>
2016-02-09 23:08:10 +0100

    Initial commit

---
+++ WCTagField/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WCTagField
+//
+//  Created by Wojciech Czekalski on 16.01.2016.
+//  Copyright © 2016 wczekalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
